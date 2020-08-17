d6f224b85ee2e74185245c9de4264ae4686e7741
NikolayTsikunov <nikolay.tsikunov@mail.ru>
2020-08-17 13:04:15 +0300

    minor changes

--- src/main/java/by/tsikunov/task1/model/service/FileReader.java
+++ src/main/java/by/tsikunov/task1/model/service/FileReader.java
@@ -3,5 +3,5 @@ package by.tsikunov.task1.model.service;
 import java.util.List;
 
 public class FileReader {
-    List
+
 }
---
+++ src/main/java/by/tsikunov/task1/model/service/main.java
@@ -0,0 +1,7 @@
+package by.tsikunov.task1.model.service;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("work");
+    }
+}
